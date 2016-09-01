c7fd7b877b5514b1d01207fc09db1a3775b35bac
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-08-31 21:22:50 -0400

    Transforms a stream of objects into GeoJSON features

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,10 @@
 var test = require('tape');
 var jgeo = require('../lib/index');
+var fs = require('fs');
+var Writable = require('stream').Writable;
+
+var venues = require('./fixtures/foursquare-venues.json').response.venues;
+var getCoordinates = (o) => [ o.location.lng, o.location.lat ];
 
 test('it converts an object to a GeoJSON feature', function (t) {
   var obj = require('./fixtures/government-dataset.json');
@@ -11,8 +16,6 @@ test('it converts an object to a GeoJSON feature', function (t) {
   t.equal(found.properties.producto, 'TORTILLA DE MAIZ', 'keeps properties');
   t.deepEqual(found.geometry.coordinates, coords, 'finds coordinates');
 
-  var venues = require('./fixtures/foursquare-venues.json').response.venues;
-  var getCoordinates = (o) => [o.location.lng, o.location.lat];
   var v = jgeo.toGeoJSON(venues[0], getCoordinates);
 
   t.equal(v.properties.location.city, 'Brooklyn', 'deep copy of properties');
@@ -20,4 +23,25 @@ test('it converts an object to a GeoJSON feature', function (t) {
   t.equal(v.properties.categories[0].name, 'Park', 'keeps properties');
 
   t.end();
+});
+
+test('it transforms a stream of objects to GeoJSON features', function (t) {
+  t.plan(90);
+
+  var isFloat = (f) => f.toString().match(/\d+\.\d+/);
+  var inNY = (s) => (['NY', 'New York'].indexOf(s) >= 0);
+  var ws = new Writable;
+
+  ws._write = function (chunk, enc, next) {
+    var obj = JSON.parse(chunk);
+
+    t.equal(obj.type, 'Feature', 'GeoJSON feature coming from the stream');
+    t.true(isFloat(obj.geometry.coordinates[1]), 'Finds it latitude');
+    t.true(inNY(obj.properties.location.state), 'Preserves properties');
+
+    next();
+  };
+
+  var rs = fs.createReadStream(__dirname + '/fixtures/foursquare-venues.json');
+  jgeo.transform(rs, 'response.venues.*', getCoordinates).pipe(ws)
 });
